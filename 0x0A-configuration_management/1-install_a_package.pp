1eb07fcb9fff2e0a34004929e6c7d88d1cd358bf
Dymond <123193636+Ufoma@users.noreply.github.com>
2024-04-20 15:07:13 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Install flask with puppet
 
 package { 'flask':
-ensure   => '2.1.0',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.0.1',
   provider => 'pip3',
 }
